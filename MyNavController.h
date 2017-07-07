4520499d7c16ec262dd6c122111e1872c752b8e9
Yura Mumryak <mumryak@gmail.com>
2017-07-07 18:51:02 +0300

    Replaces ViewController for NavigationController, and added 1 line of code to AppDelegate.m

--- MyNavController.h
+++ MyNavController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MyNavController.h
 //  TrainTVNavigationAndCustomCell
 //
 //  Created by Yura Mumryak on 07.07.17.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface MyNavController : UINavigationController
 
 @end
-
